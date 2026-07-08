e498c4a898067302c6f14e44ad44c0112a22b8bc
alex <alexanderetmekchyan@gmail.com>
2026-07-08 16:28:39 +0400

    addition of cartModal

--- force-app/main/default/lwc/cartModal/cartModal.js
+++ force-app/main/default/lwc/cartModal/cartModal.js
@@ -1,3 +1,26 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class CartModal extends LightningElement {}
+export default class CartModal extends LightningElement {
+  @api cartItems = [];
+
+  get total() {
+    return this.cartItems.reduce(
+      (sum, line) => sum + line.Price__c * line.amount,
+      0
+    );
+  }
+
+  handleClose() {
+    this.dispatchEvent(new CustomEvent("close"));
+  }
+
+  handleCheckout() {
+    this.dispatchEvent(new CustomEvent("checkout"));
+  }
+
+  handleRemove(event) {
+    this.dispatchEvent(
+      new CustomEvent("remove", { detail: event.target.dataset.id })
+    );
+  }
+}
